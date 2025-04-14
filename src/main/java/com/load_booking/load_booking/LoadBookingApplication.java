d5ad070ef3f07a70ea84128ef6f47bb28ba9e781
shirshakk <kshirshak76@gmail.com>
2025-04-14 17:39:14 +0530

    Project Initiated

---
+++ src/main/java/com/load_booking/load_booking/LoadBookingApplication.java
@@ -0,0 +1,13 @@
+package com.load_booking.load_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/load_booking/load_booking/LoadBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.load_booking.load_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
